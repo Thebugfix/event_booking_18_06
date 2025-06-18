b3af7be5aff422b7fe63f2751ce34eca9644cfeb
Naved Shaikh <shaikhnaved0001@gmail.com>
2025-06-18 13:01:20 +0530

    third commit

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use("/api/bookings", bookingRoutes)
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the Event Booking API" })
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -44,4 +45,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 // export for vecel deployment
-module.exports = app
